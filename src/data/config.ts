981ef9765d14b0d9593c050961b2c3409a936558
Lasse Pehkonen <lasse.p.pehkonen@gmail.com>
2024-12-28 16:23:14 +0200

    commit 0.1

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,1 +1,1 @@
-export const SITE_URL = "https://astro-decapcms-starter.netlify.app/";
+export const SITE_URL = "https://singular-belekoy-185ac4.netlify.app/";
--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -11,22 +11,14 @@ type Presentation = {
 };
 
 const presentation: Presentation = {
-  mail: "gxanshu@yahoo.com",
-  title: "Hi, I’m Anshu 👋",
+  mail: "",
+  title: "",
   description:
-    "Howdy, i'm a *indian software programmer* with over *5 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
+    "",
   socials: [
     {
-      label: "Twiiter",
-      link: "https://twitter.com/gxanshu",
-    },
-    {
-      label: "Bento",
-      link: "https://bento.me/gxanshu",
-    },
-    {
-      label: "Github",
-      link: "https://github.com/gxanshu",
+      label: "",
+      link: "",
     },
   ],
 };
