7a01fd62674301657e2090bd3219061222e76d43
Lasse Pehkonen <lasse.p.pehkonen@gmail.com>
2024-12-30 22:31:14 +0200

    V0.1.7

--- src/content/config.ts
+++ src/content/config.ts
@@ -19,7 +19,29 @@ const pagesCollection = defineCollection({
     description: z.string(),
     isPublish: z.boolean(),
     isDraft: z.boolean().default(false),
+    body: z.array(
+      z.union([
+        z.object({
+          type: z.literal("text"),
+          content: z.string(),
+        }),
+        z.object({
+          type: z.literal("image"),
+          src: z.string(),
+          alt: z.string(),
+        }),
+        z.object({
+          type: z.literal("columns"),
+          columns: z.array(
+            z.object({
+              content: z.string(),
+            })
+          ),
+        }),
+      ])
+    ),
   }),
 });
+
 
 export const collections = { posts: postsCollection, pages: pagesCollection };
